09e6734917f05f4fbe5430e74c166867fa792dee
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-18 13:44:52 +1300

    Update BooleanExample.java

--- BooleanExample.java
+++ BooleanExample.java
@@ -6,9 +6,9 @@ public class BooleanExample {
 		boolean b1 = true;
 		boolean b2 = false;
 		
-//		boolean b3 = False; 대문자여서 X
-//		boolean b4 = "false"; 문자열이여서 X
-//		boolean b5 = 0; int이여서 X 
+//		boolean b3 = False; Since the first letter is in capital (X)
+//		boolean b4 = "false";  (X)
+//		boolean b5 = 0; number so (X)
 		System.out.println(b1 == b2);
 		System.out.println(b1 != b2);
 
