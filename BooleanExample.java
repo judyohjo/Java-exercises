a7dca59ef24f58b3dd391d912ace13e2ecb5a9a7
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-18 09:43:35 +0900

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,17 @@
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+//		boolean b3 = False; 대문자여서 X
+//		boolean b4 = "false"; 문자열이여서 X
+//		boolean b5 = 0; int이여서 X 
+		System.out.println(b1 == b2);
+		System.out.println(b1 != b2);
+
+	}
+
+}
