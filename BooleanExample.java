8648291658b2f5612eb3fd45b71220a6c8388911
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-21 09:20:11 +0900

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,15 @@
+//in java, filename and class name should be the same (eg. BooleanExample)
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+//		boolean b3 = False; (X) because this is in capital
+//      boolean b4 = "false"; (X) becuase it's a string
+//		boolean b5 = 0; (X) because this is int
+
+	}
+
+}
