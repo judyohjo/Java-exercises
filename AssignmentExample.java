a0fa0e5da61177caed1701a5829c977774fae9b1
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-29 16:10:17 +1300

    Update AssignmentExample.java

--- AssignmentExample.java
+++ AssignmentExample.java
@@ -17,10 +17,10 @@ public class AssignmentExample {
 		a *= 6;
 		System.out.println(a);
 		
-		a /= 5; //a에서 5를 나눈 몫을 저장하라. 
+		a /= 5; 
 		System.out.println(a);
 		
-		a %= 3; //a에서 3을 나눈 나머지를 저장하라.
+		a %= 3; 
 		System.out.println(a);
 
 	}
