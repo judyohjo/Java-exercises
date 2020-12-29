3a4146938b7d0c80aff463a1735920bd8150c729
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-29 12:09:46 +0900

    Add files via upload

---
+++ AssignmentExample.java
@@ -0,0 +1,28 @@
+
+public class AssignmentExample {
+
+	public static void main(String[] args) {
+		
+		int a = 5, b = 5;
+		
+		a += 3; // a = a + 3;
+		b =+ 3; // b = + 3;
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		
+		System.out.println("----------------");
+		a -= 4;
+		System.out.println(a);
+		
+		a *= 6;
+		System.out.println(a);
+		
+		a /= 5; //a에서 5를 나눈 몫을 저장하라. 
+		System.out.println(a);
+		
+		a %= 3; //a에서 3을 나눈 나머지를 저장하라.
+		System.out.println(a);
+
+	}
+
+}
