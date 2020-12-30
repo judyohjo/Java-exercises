16598b07fa04aa0f920347e04510fd70c6be95d3
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-30 14:48:31 +1300

    Update ConditionalExample.java

--- ConditionalExample.java
+++ ConditionalExample.java
@@ -3,13 +3,11 @@ public class ConditionalExample {
 
 	public static void main(String[] args) {
 		
-		//-4 ~ 5까지의 정수 난수값을 발생.
 		int r = 5 - (int)(Math.random() * 10);
-		System.out.println("랜덤값: " + r );
+		System.out.println("Random value: " + r );
 		
-		//3항 조건연산자 (조건식 ? 연산식1 : 연산식2)
 		int abs = (r >= 0) ? r: -r;
-		System.out.println(r + "의 절대값은: " + abs);
+		System.out.println(r + "'s absolute value: " + abs);
 
 	}
 
