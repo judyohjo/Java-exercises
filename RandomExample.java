aab2bc3be6c7553b2408cbf3cc836112fc37f815
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-31 16:02:42 +1300

    Update RandomExample.java

--- RandomExample.java
+++ RandomExample.java
@@ -3,23 +3,20 @@ public class RandomExample {
 
 	public static void main(String[] args) {
 		
-		//난수 (랜덤값)을 발생시키는 메서드 random()
-		//0.0이상 1.0미만의 랜덤 실수값을  (real value) 가져옴. 
 		double r = Math.random();
-		System.out.println("랜덤값: " + r);
+		System.out.println("Random value: " + r);
 		
-		//정수 난수 생성하기
 		int rn = (int)(r * 10);
-		System.out.println("정수 난수값: " + rn);
+		System.out.println("Integer random number: " + rn);
 		System.out.println("-------------------");
 		
-		System.out.println("*** 신년 여행지 선정! ***");
+		System.out.println("*** Travelling places to go ***");
 		if (r > 0.66) {
-			System.out.println("모로코로 갑시다!!");
+			System.out.println("Let's go to New Zealand!!");
 		} else if (r > 0.33) {
-			System.out.println("상하이로 갑시다!!");
+			System.out.println("Let's go to Australia!!");
 		} else {
-			System.out.println("집에서 놉시다!!");
+			System.out.println("Let's stay home!!");
 		}
 
 	}
