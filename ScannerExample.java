4547d4cc23243a6cbb475612d27853b607d43615
judyohjo <68041382+judyohjo@users.noreply.github.com>
2021-01-01 14:31:38 +1300

    Update ScannerExample.java

--- ScannerExample.java
+++ ScannerExample.java
@@ -4,23 +4,19 @@ public class ScannerExample {
 
 	public static void main(String[] args) {
 		
-		//1. 스캐너 객체 생성
 		Scanner scan = new Scanner(System.in);
 		
-		//2. 스캐너 클래스가 가지고 있는 입력메서드을을 
-		//사용하여 데이터를 입력받음.
-		System.out.println("이름을 입력하세요.");
+		System.out.println("Insert your name.");
 		System.out.print("> ");
 		String name = scan.next();
 		
-		System.out.println("나이를 입력하세요.");
+		System.out.println("Insert your age.");
 		System.out.print("> ");
 		int age = scan.nextInt();
 		System.out.println("--------------------");
 		
-		System.out.println("당신의 이름은 " + name + "이고 나이는 " + age + "세입니다.");
+		System.out.println("Your name is " + name + "and you are " + age + "years old.");
 		
-		//3. 스캐너의 메모리 지원을 반납한다.
 		scan.close();
 	}
 
