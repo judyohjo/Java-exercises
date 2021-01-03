c5cf33f041a0b4092dd62c93945b3eedf9c25e32
judyohjo <68041382+judyohjo@users.noreply.github.com>
2021-01-03 16:32:36 +1300

    Update IfExample2.java

--- IfExample2.java
+++ IfExample2.java
@@ -6,27 +6,27 @@ public class IfExample2 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("점수를 입력하세요.");
+		System.out.println("Insert your score.");
 		System.out.print("> ");
 		int point = sc.nextInt(); 
 		 
-		if(point >= 90) {
+		if(point >= 85) {
 			if(point > 100) {
-				System.out.println("점수를 잘못 입력했습니다.");
-			} else if(point >= 95) {
-				System.out.println("당신의 학점은 A+입니다.");
+				System.out.println("You didn't submit the correct score.");
+			} else if(point >= 90) {
+				System.out.println("Your grade is an A+.");
 			} else {
-				System.out.println("당신의 학점은 A입니다.");
+				System.out.println("Your grade is an A.");
 			}
 		} else if(point >= 80) {
-			System.out.println("당신의 학점은 B입니다.");
+			System.out.println("Your grade is an A-.");
 		} else if(point >= 70) {
-			System.out.println("당신의 학점은 C입니다.");
-		} else if(point >= 60) {
-			System.out.println("당신의 학점은 D입니다.");
+			System.out.println("Your grade is a B.");
+		} else if(point >= 50) {
+			System.out.println("Your grade is a C.");
 		} else {
-			System.out.println("당신의 학점은 F입니다.");
-			System.out.println("당신은 재수강대상입니다.");
+			System.out.println("Your grade is a F.");
+			System.out.println("You need to retake this paper.");
 		}
 
 	}
