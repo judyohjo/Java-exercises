111a9d1f68fcd4c66e9996a39dd04c95de140f5b
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2021-01-14 04:18:37 +1300

    Update WhileExample1.java

--- WhileExample1.java
+++ WhileExample1.java
@@ -3,16 +3,15 @@ public class WhileExample1 {
 
 	public static void main(String[] args) {
 		
-		// 1~10까지의 누적합계를 구하는 로직
-		int sum = 0; //합계를 저장할 변수
-		int n = 1; // 제어변수: 반복문의 횟수를 제어할 변수.
+		int sum = 0;
+		int n = 1;
 		
 		while(n <= 100) {
 			sum += n;
-			n++; //제어변수 조작을 통해 반복문이 언젠가 false가 나오게 함.
+			n++;
 		}
 		
-		System.out.println("1부터 10까지의 누적합: " + sum);
+		System.out.println("Total sum: " + sum);
 		
 
 	}
