553ef7c5d1b6fd9456382698c78978b1fd749817
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2021-01-17 10:46:33 +0900

    Add files via upload

---
+++ ForExample1.java
@@ -0,0 +1,14 @@
+
+public class ForExample1 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		for (int n = 5; n <= 35; n++) {
+			sum += n;
+		}
+		System.out.println("누적합: " + sum);
+
+	}
+
+}
