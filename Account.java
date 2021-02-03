51e8e9f7938f2838cc453d6160777348452f74c1
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2021-02-03 15:17:38 +1300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,40 @@
+
+public class Account {
+	
+	/*
+	 * 1. 멤버변수는 예금주아름(name : String), 비밀번호(password: 
+	 * int), 잔액(balance: int)로 선언하세요.
+	 * 2. 생성자는 3가지 멤버변수를 모두 매개값으로 받아 초기화하는 생성자를
+	 * 선언하세요.
+	 * 3. 메서드는 입금기능 메서드 (deposit : void), 출금기능 메서드
+	 * (withDraw : void), 잔액 조회기능 메서드 (getBalance : int) 
+	 * 3가지를 선언하세요.
+	 */
+	
+	//객체의 속성(멤버변수)
+	String name;
+	int password;
+	int balance;
+	
+	//생성자 선언
+	Account(String aName, int pw, int bal) {
+		name = aName;
+		password = pw;
+		balance = bal;
+	}
+	
+	//입금 기능
+	void deposit(int money) {
+		balance += money;
+	}
+	
+	//출금 기능
+	void withDrawMoney(int money) {
+		balance -= money; //balance = balance - money
+	}
+	
+	//잔액 조회 기능
+	int getBalance() {
+		return balance;
+	}
+}
