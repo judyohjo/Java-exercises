f1c12b0463932614e513095bff3da637a619be7c
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2021-02-05 02:27:57 +1300

    Update 23. Account.java

--- 23. Account.java
+++ 23. Account.java
@@ -2,8 +2,8 @@
 public class Account {
 	
 	/*
-	 * 1. 멤버변수는 예금주아름(name : String), 비밀번호(password: 
-	 * int), 잔액(balance: int)로 선언하세요.
+	 * 1. Class parameters are the name of the account holdee(name : String), account password(password: 
+	 * int), and account balance (balance: int).
 	 * 2. 생성자는 3가지 멤버변수를 모두 매개값으로 받아 초기화하는 생성자를
 	 * 선언하세요.
 	 * 3. 메서드는 입금기능 메서드 (deposit : void), 출금기능 메서드
