631b09351c02d64e1fd4e8fc8f1a6085c197f63f
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-08 00:20:24 +0900

    Create lab0q1.java

---
+++ lab0q1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lab00 {
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter the first integer: ");
+      System.out.println("Enter the second integer: ");
+      System.out.println("Enter the third integer: ");
+      
+      int num1 = input.nextInt();
+      System.out.printf("%5d %4d %4d%n", num1, square(num1), cube(num1));
+      
+      int num2 = input.nextInt();
+      System.out.printf("%5d %4d %4d%n", num2, square(num2), cube(num2));
+      
+      int num3 = input.nextInt();
+      System.out.printf("%5d %4d %4d%n", num3, square(num3), cube(num3));
+      
+  }
+  public static int square(int num) {
+	  return num * num;
+  }
+  public static int cube(int num) {
+	  return num * num * num;
+  }
+}
