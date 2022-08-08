8522840cebfa29f59366d54247a876d859750156
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-09 00:42:36 +0900

    Create lab0q2.java

---
+++ lab0q2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Lab00 {
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter an integer: ");
+       int num1 = input.nextInt();
+       double d = Math.sqrt(num1);
+       System.out.printf("Square root of %d = %.2f", num1, d);
+   }
+
+}
