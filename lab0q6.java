34469d0300067797d94373036fcb67a942fbae3c
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-10 00:30:36 +0900

    Create lab0q6.java

---
+++ lab0q6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lab00Q6 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number of inches: ");
+		int inch = input.nextInt();
+		int feet = inch/12;
+		int remainder = inch % 12;
+		if(feet != 1 && remainder != 1) {
+			System.out.printf("That is %d feet and %d inches.", feet, remainder);
+		} else if (feet != 1 && remainder == 1){
+			System.out.printf("That is %d feet and %d inch.", feet, remainder);
+		} else {
+			System.out.printf("That is %d foot and %d inches.", feet, remainder);
+		}
+	}
+	
+	
+}
