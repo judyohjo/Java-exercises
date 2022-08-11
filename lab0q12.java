5e6ee7182eefcbf9f0e52ae4832d8034ce6d6f5c
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-12 00:38:05 +0900

    Create lab0q12.java

---
+++ lab0q12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfN {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter a positive integer: ");
+      int num = input.nextInt();
+      
+      double sum = num * (num+1.0)/2.0;
+      
+      System.out.printf("The sum of the first %d positive integers is %.0f.", num, sum);
+      
+      
+   } // end main
+} // end class
