754f7dd2610f2db43cc008297f31ca73d3d030be
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-13 00:09:58 +0900

    Create lab1q2.java

---
+++ lab1q2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import java.lang.*;
+public class Lab01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a sentence: ");
+		String phrase = input.nextLine();
+		
+		
+		String lowerphrase = phrase.toLowerCase();
+		
+		int letters=0;
+		int digits=0;
+		
+		for(int i=0; i<lowerphrase.length(); i++) {
+			char c = lowerphrase.charAt(i);
+			if(Character.isLetter(c)) {
+				letters++;
+			} else if(Character.isDigit(c)) {
+				digits++;
+			}
+		}
+	
+		System.out.printf("The number of letters is %d and the number of digits is %d.", letters, digits);
+	}
+}
