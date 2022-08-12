824444f2651ef84cbd1f5470b41fdcd67f565e1e
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-13 00:09:11 +0900

    Create lab1q1.java

---
+++ lab1q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab00Color {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your favourite colour?");
+		String colour = input.next();
+		String lowercolour = colour.toLowerCase();
+		
+		int count = lowercolour.length();
+		
+		int i;
+		
+		for(i=0; i<count; i++) {
+			System.out.printf("%s", "#");
+		}
+		
+		System.out.printf("\n%s\n", lowercolour);
+		
+		for(i=0; i<count; i++) {
+			System.out.printf("%s", "#");
+		}
+	
+	}
+}
