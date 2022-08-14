ffcfc155f054c8b672250e9e4e337cfc7667ff6d
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-15 00:37:53 +0900

    Update lab0q4.java

--- lab0q4.java
+++ lab0q4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab00Q04 {
+public class printX {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a number: ");
