53b1f66e48d0e2af5891f71b4faa4c8af0f6db0a
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-15 00:38:14 +0900

    Update lab0q3.java

--- lab0q3.java
+++ lab0q3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LabTriangle{
+public class AreaOfTriangle{
    public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	System.out.println("Enter the height of a triangle: ");
