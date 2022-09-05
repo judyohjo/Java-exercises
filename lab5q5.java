3e51ff591a3dee20fafc5a31d55d27eb51ce411f
Judy Oh <68041382+judyohjo@users.noreply.github.com>
2022-09-06 08:59:20 +0900

    Create lab5q5.java

---
+++ lab5q5.java
@@ -0,0 +1,21 @@
+class Triathlete implements Swim {
+    public Triathlete() {
+        super();
+    }
+    
+    public void fastSwim() {
+        System.out.println("fastSwim");
+    }
+    
+    public void slowSwim() {
+        System.out.println("slowSwim");
+    }
+    
+    public void run() {
+        System.out.println("run");
+    }
+    
+    public void cycle() {
+        System.out.println("cycle");
+    }
+}
